c3fb20cff58325d2698753bdf7b95e31f4bc3eab
Tzachi Dar <tzachi.dar@gmail.com>
2021-06-20 23:55:34 +0300

    2 fixes for libre: 1) Make sure we always use only the first 0x158 bytes of the data. 2) In the case of libre2, also allow flags == 0 as a legal packet.
    Still needs testing.
    
    Signed-off-by: Tzachi Dar <tzachi.dar@gmail.com>

--- app/src/main/java/com/eveningoutpost/dexdrip/utils/LibreTrendPoint.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/utils/LibreTrendPoint.java
@@ -33,8 +33,8 @@ public class LibreTrendPoint {
     public boolean isError() {
         boolean ret = rawSensorValue == 0;
         if (source == GlucoseData.DataSource.FRAM) {
-            // row below commented out to avoid NFC reading error
-            //ret |= flags != 800;
+            // For libre2, it seems that flags ==0 is also a valid value.
+            ret |= (flags != 800) && (flags != 0);
         } else if (source == GlucoseData.DataSource.BLE) {
             // Condition already checked (rawSensorValue == 0)
         } else {
