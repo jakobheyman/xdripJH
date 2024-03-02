bac961b460a227d0d55883ca5dc904035166604a
Jakob Heyman <jakob.heyman@gu.se>
2024-03-02 23:15:20 +0100

    fix to show temp basals from AAPS properly

--- app/src/main/java/com/eveningoutpost/dexdrip/wearintegration/ExternalStatusService.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/wearintegration/ExternalStatusService.java
@@ -126,7 +126,7 @@ public class ExternalStatusService extends IntentService {
     // extract a TBR percentage from a status line string.
     public static String getTBR(final String statusLine) {
         if (JoH.emptyString(statusLine)) return "";
-        val pattern = Pattern.compile(".*([^0-9]|^)([0-9]+%)", Pattern.DOTALL); // match last of any number followed by %
+        val pattern = Pattern.compile(".*([^0-9]|^)([0-9]+ %)", Pattern.DOTALL); // match last of any number followed by %
         val matcher = pattern.matcher(statusLine);
         val matches = matcher.find();       // was at least one found?
 
@@ -139,7 +139,7 @@ public class ExternalStatusService extends IntentService {
 
     public static String getAbsoluteBR(final String statusLine) {
         if (JoH.emptyString(statusLine)) return "";
-        val pattern = Pattern.compile(".*(^|[^0-9.,])([0-9.,]+U/h)", Pattern.DOTALL); // match last of any number followed by units per hour
+        val pattern = Pattern.compile(".*(^|[^0-9.,])([0-9.,]+ U/h)", Pattern.DOTALL); // match last of any number followed by units per hour
         val matcher = pattern.matcher(statusLine);
         val matches = matcher.find();       // was at least one found?
 
